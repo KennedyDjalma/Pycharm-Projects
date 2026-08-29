eb91367fd117f09f3e84f4275f611bda61732718
Kennedy Djalma <kennedydjalmadesantana@hotmail.com>
2026-08-29 15:47:36 -0300

    Matriz em Python

---
+++ CursoemVideo/PythonExercicios/ex086.py
@@ -0,0 +1,16 @@
+''' EXERECICIO 86
+CRIE UM PROGRAMA QUE CRIE UMA MATRIZ DE DIMENSAO 3X3
+E PREENCHA COM VALORES LIDO PELO TECLADO.
+NO FINAL, MOSTRE A MATRIZ NA TELA, COM A FORMAÇAÕ CORRETA.
+'''
+#https://www.youtube.com/watch?v=EGmlFdwD4C4&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=106
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        matriz[linha] [coluna] = int(input(f'Digite um valor para [{linha}][{coluna}]: '))
+
+print('-='*30)
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        print(f'[{matriz[linha][coluna]:^11}]', end='') #:^11  FORMATAÇÃO DE ESPAÇOS
+    print()
